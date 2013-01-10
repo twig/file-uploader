8597688ee1eb643959d406cfb8b828a75278a6f5
twig <twig.nguyen@gmail.com>
2013-01-10 15:19:46 +1100

    re-enable the drag/drop functionality if the user drags over more than 1 file when multiple is off

--- client/js/dnd.js
+++ client/js/dnd.js
@@ -74,6 +74,7 @@ qq.DragAndDrop = function(o) {
 
         if (dataTransfer.files.length > 1 && !options.multiple) {
             options.callbacks.error('tooManyFilesError', "");
+            dz.dropDisabled(false);
         }
         else {
             droppedFiles = [];
